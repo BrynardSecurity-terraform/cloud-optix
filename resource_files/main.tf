9b6d68b973fb95aa5cf8786a4cfb6bc863337fc0
RalphBrynard <ralph@thebrynards.com>
2022-02-02 14:52:41 -0500

    Added provider aliases to main.tf

--- resource_files/main.tf
+++ resource_files/main.tf
@@ -2,6 +2,26 @@ provider "aws" {
   region = var.AWS_DEFAULT_REGION
 }
 
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "us-west-1"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
+
 data "aws_caller_identity" "current" {}
 
 output "account_id" {
