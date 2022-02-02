73c8970319bbf227d50cf3c767e599024fae9c29
RalphBrynard <ralph@thebrynards.com>
2022-02-02 14:49:25 -0500

    corrected flowlog_regions in vars.tf

--- resource_files/vars.tf
+++ resource_files/vars.tf
@@ -25,19 +25,7 @@ variable "FLOWLOG_REGIONS" {
     "us-west-1",
     "us-west-2",
     "us-east-1",
-    "us-east-2",
-    "eu-west-1",
-    "eu-west-2",
-    "eu-central-1",
-    "ap-south-1",
-    "ap-southeast-1",
-    "ap-southeast-2",
-    "ap-northeast-1",
-    "ap-northeast-2",
-    "sa-east-1",
-    "ca-central-1",
-    "eu-west-3",
-    "eu-north-1",
+    "us-east-2"
   ]
 }
 variable "FLOW_ONE_REGION_VALUE" { default = "us-west-1"  }
